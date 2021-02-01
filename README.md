020880ed331b3345489964b56afc5ea0b3c8e481
Xu Zili <stefango@qq.com>
2021-02-01 23:38:41 +0800

    antd-mobile init

---
+++ README.md
@@ -0,0 +1,2 @@
+# TS-APP
+> 使用 create-react-app、antd-mobile、typescript 搭建
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Tab from './components/Tab';
+
+function App() {
+  return (
+    <div className="App">
+      <Tab />
+    </div>
+  );
+}
+
+export default App;
